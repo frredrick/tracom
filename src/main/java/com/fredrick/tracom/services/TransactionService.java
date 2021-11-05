31c8952c78f8ddaf0f4294440e88e466539e41e9
Fredrick Otieno <otienofredrick51@gmail.com>
2021-11-05 13:59:03 +0300

    last commit

--- src/main/java/com/fredrick/tracom/services/TransactionService.java
+++ src/main/java/com/fredrick/tracom/services/TransactionService.java
@@ -42,54 +42,54 @@ public class TransactionService {
 
 		// validating fields
 		if (transaction == null) {
-			return new ResponseEntity<String>("Add a request body", HttpStatus.OK);
+			return new ResponseEntity<String>("Add a request body", HttpStatus.BAD_REQUEST);
 
 		}
 		if (transaction.getBankBranch() == null || transaction.getBankBranch().isEmpty()) {
-			return new ResponseEntity<String>("Bank branch  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Bank branch  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 
 		Branches validateBranch = branchesServices.getSingleBranch(transaction.getBankBranch());
 		if (validateBranch.getBranchName() == null || validateBranch.getBranchName().isEmpty()) {
 
-			return new ResponseEntity<String>("Bank branch  is not registered Please Add!", HttpStatus.OK);
+			return new ResponseEntity<String>("Bank branch  is not registered Please Add!", HttpStatus.BAD_REQUEST);
 		}
 
 		if (transaction.getCountryCode() == null || transaction.getCountryCode().isEmpty()) {
-			return new ResponseEntity<String>("Country  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Country  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		Countries validateCountry = countriesService.getCountry(transaction.getCountryCode());
 		if (validateCountry.getCountryName() == null || validateCountry.getCountryName().isEmpty()) {
-			return new ResponseEntity<String>("Country Code  is not registered please add !", HttpStatus.OK);
+			return new ResponseEntity<String>("Country Code  is not registered please add !", HttpStatus.BAD_REQUEST);
 		}
 
 		if (transaction.getDeviceSerialnumber() == null || transaction.getDeviceSerialnumber().isEmpty()) {
-			return new ResponseEntity<String>(" Device Serial Number  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>(" Device Serial Number  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 
 		Devices validateDevice = devicesService.getDevices(transaction.getDeviceSerialnumber());
 		if (validateDevice.getSerialNumber() == null || validateDevice.getSerialNumber().isEmpty()) {
-			return new ResponseEntity<String>(" Device Serial is not yet registered please add!", HttpStatus.OK);
+			return new ResponseEntity<String>(" Device Serial is not yet registered please add!", HttpStatus.NOT_FOUND);
 		}
 
 		if (transaction.getClientCode() == null || transaction.getClientCode().isEmpty()) {
-			return new ResponseEntity<String>("Client Code   is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Client Code   is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		Clients validateClient = clientsService.getClient(transaction.getClientCode());
 		if (validateClient.getClientCode() == null || validateClient.getClientCode().isEmpty()) {
-			return new ResponseEntity<String>("Client not registered  please add a registered client!", HttpStatus.OK);
+			return new ResponseEntity<String>("Client not registered  please add a registered client!", HttpStatus.NOT_FOUND);
 
 		}
 		if (transaction.getModifiedBy() == null || transaction.getModifiedBy().isEmpty()) {
-			return new ResponseEntity<String>("Modified by  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Modified by  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		if (!allTransactions.isEmpty()) {
-			return new ResponseEntity<String>("device already assigned", HttpStatus.OK);
+			return new ResponseEntity<String>("device already assigned", HttpStatus.CONFLICT);
 		}
 
 		// saving Device
@@ -105,55 +105,55 @@ public class TransactionService {
 	public ResponseEntity<String> updateTransaction(final Transactions transaction) {
 		// validating fields
 		if (transaction == null) {
-			return new ResponseEntity<String>("Add a request body", HttpStatus.OK);
+			return new ResponseEntity<String>("Add a request body", HttpStatus.BAD_REQUEST);
 
 		}
 		if (transaction.getBankBranch() == null || transaction.getBankBranch().isEmpty()) {
-			return new ResponseEntity<String>("Bank branch  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Bank branch  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 
 		Branches validateBranch = branchesServices.getSingleBranch(transaction.getBankBranch());
 		if (validateBranch.getBranchName() == null || validateBranch.getBranchName().isEmpty()) {
 
-			return new ResponseEntity<String>("Bank branch  is not registered Please Add!", HttpStatus.OK);
+			return new ResponseEntity<String>("Bank branch  is not registered Please Add!", HttpStatus.NOT_FOUND);
 		}
 
 		if (transaction.getCountryCode() == null || transaction.getCountryCode().isEmpty()) {
-			return new ResponseEntity<String>("Country  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Country  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		Countries validateCountry = countriesService.getCountry(transaction.getCountryCode());
 		if (validateCountry.getCountryName() == null || validateCountry.getCountryName().isEmpty()) {
-			return new ResponseEntity<String>("Country Code  is not registered please add !", HttpStatus.OK);
+			return new ResponseEntity<String>("Country Code  is not registered please add !", HttpStatus.NOT_FOUND);
 		}
 
 		if (transaction.getDeviceSerialnumber() == null || transaction.getDeviceSerialnumber().isEmpty()) {
-			return new ResponseEntity<String>(" Device Serial Number  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>(" Device Serial Number  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 
 		Devices validateDevice = devicesService.getDevices(transaction.getDeviceSerialnumber());
 		if (validateDevice.getSerialNumber() == null || validateDevice.getSerialNumber().isEmpty()) {
-			return new ResponseEntity<String>(" Device Serial is not yet registered please add!", HttpStatus.OK);
+			return new ResponseEntity<String>(" Device Serial is not yet registered please add!", HttpStatus.NOT_FOUND);
 		}
 
 		if (transaction.getClientCode() == null || transaction.getClientCode().isEmpty()) {
-			return new ResponseEntity<String>("Client Code   is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Client Code   is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		Clients validateClient = clientsService.getClient(transaction.getClientCode());
 		if (validateClient.getClientCode() == null || validateClient.getClientCode().isEmpty()) {
-			return new ResponseEntity<String>("Client not registered  please add a registered client!", HttpStatus.OK);
+			return new ResponseEntity<String>("Client not registered  please add a registered client!", HttpStatus.NOT_FOUND);
 
 		}
 		if (transaction.getModifiedBy() == null || transaction.getModifiedBy().isEmpty()) {
-			return new ResponseEntity<String>("Modified by  is Empty!", HttpStatus.OK);
+			return new ResponseEntity<String>("Modified by  is Empty!", HttpStatus.BAD_REQUEST);
 
 		}
 		if (!transactionRepository.existsById(transaction.getId())) {
 			return new ResponseEntity<String>("Transaction with Id : " + transaction.getId() + " does not exists",
-					HttpStatus.OK);
+					HttpStatus.NOT_FOUND);
 
 		}
 
@@ -167,7 +167,7 @@ public class TransactionService {
 		if (res != null) {
 			return new ResponseEntity<String>("Updated Successfully", HttpStatus.OK);
 		}
-		return new ResponseEntity<String>("failed", HttpStatus.OK);
+		return new ResponseEntity<String>("failed", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	public ResponseEntity<String> deleteTransaction(final Long id) {
@@ -176,7 +176,7 @@ public class TransactionService {
 			transactionRepository.deleteById(null);
 			return new ResponseEntity<String>("Deleted successfully", HttpStatus.OK);
 		}
-		return new ResponseEntity<String>("failed to delete transaction with id :" + id + "", HttpStatus.OK);
+		return new ResponseEntity<String>("failed to delete transaction with id :" + id + "", HttpStatus.NOT_FOUND);
 
 	}
 
